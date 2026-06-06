b113a79d69e8a59b49622d5fcaadbec0511fec7d
Adrian-24914 <pen24914@uvg.edu.gt>
2026-06-06 09:44:32 -0600

    feat: create function to handle the resize of the window

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const canvas = document.getElementById('experience-canvas');
+const sizes ={
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
+
+
+
+const renderer = new THREE.WebGLRenderer( { canvas :canvas } );
+renderer.setSize( sizes.width, sizes.height );
+
+renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function onWindowResize() {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( sizes.width, sizes.height );
+}
+
+window.addEventListener( 'resize', onWindowResize );
+
+function animate( time ) {
+
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+
+  renderer.render( scene, camera );
+
+}
