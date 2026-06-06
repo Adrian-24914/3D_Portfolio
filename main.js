c4d21ac04106603ba4d8cc8135cacc03be9b6786
Adrian-24914 <pen24914@uvg.edu.gt>
2026-06-06 10:08:47 -0600

    feat: load the 3D model for the portfolio

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const canvas = document.getElementById('experience-canvas');
@@ -11,6 +12,18 @@ const sizes ={
 const renderer = new THREE.WebGLRenderer( { canvas: canvas } );
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
+
+const loader = new GLTFLoader();
+
+loader.load( './Portafolio.glb', function ( gltf ) {
+
+  scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
 
 const camera = new THREE.PerspectiveCamera(
     75,
